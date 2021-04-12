b38d00f8d9e75af1b629fc8fdedf50776d6d503d
ShuichiroTsuda <shuichiro715@outlook.jp>
2021-04-12 14:34:32 +0900

    Add import stepper

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,13 @@
 #include <mcp_can.h>
 #include <SPI.h>
 
-MCP_CAN CAN(10);
+#define MOTION_SENSOR_PIN 1
 
 void setup()
 {
-  Serial.begin(115200);
-
-START_INIT:
-
-  if (CAN_OK == CAN.begin(CAN_500KBPS)) // init can bus : baudrate = 500k
-  {
-    Serial.println("CAN BUS Shield init ok!");
-  }
-  else
-  {
-    Serial.println("CAN BUS Shield init fail");
-    Serial.println("Init CAN BUS Shield again");
-    delay(100);
-    goto START_INIT;
-  }
 }
 
 void loop()
 {
+  int motionSensorValue = analogRead(MOTION_SENSOR_PIN);
 }
