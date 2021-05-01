95585c6e4924f1797cf06937de46ed2460972b33
ShuichiroTsuda <shuichiro715@outlook.jp>
2021-05-02 07:27:54 +0900

    Fix serial logger

--- src/serial_logger.cpp
+++ src/serial_logger.cpp
@@ -8,7 +8,7 @@ SerialLogger::SerialLogger()
 void SerialLogger::log(double apps1Value, double apps2Value, double tps1Value, double tps2Value, bool isPlausible, String errStr)
 {
     String logStr = "APPS1: " + String(apps1Value) +
-                    "APPS2: " + String(apps2Value) +
+                    ", APPS2: " + String(apps2Value) +
                     ", TPS1: " + String(tps1Value) +
                     ", TPS2: " + String(tps2Value) +
                     ", Plausible: " + String(isPlausible) +
