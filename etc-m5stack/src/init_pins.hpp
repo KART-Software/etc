7cf857b27d4e48fad61e9215ec421a9cd7539fd0
ORyomei <o.ryomei1020@gmail.com>
2021-07-23 23:44:52 +0900

    Delete unnecessary lines

--- etc-m5stack/src/init_pins.hpp
+++ etc-m5stack/src/init_pins.hpp
@@ -12,13 +12,9 @@
 #define TPS_1_PIN ARDUINO_A2
 #define TPS_2_PIN ARDUINO_A3
 
-#define STEPPER_ENABLE_PIN 19
-
-// #define STEPPER_OUTPUT_PIN_1 16
-// #define STEPPER_OUTPUT_PIN_2 17
 #define STEPPER_DIR_PIN 25
 #define STEPPER_STEP_PIN 26
-#define STEPPER_ENABLE_PIN 1
+#define STEPPER_ENABLE_PIN 19
 
 void initPins();
 
