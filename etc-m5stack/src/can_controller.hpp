6ef9268133d4f7b1d4403bdeb941bbf121b66e69
ORyomei <o.ryomei1020@gmail.com>
2021-09-21 21:51:59 +0900

    Add CAN controller (WIP)

---
+++ etc-m5stack/src/can_controller.hpp
@@ -0,0 +1,10 @@
+#ifndef _CAN_CONTROLLER_H_
+#define _CAN_CONTROLLER_H_
+#include <M5Stack.h>
+#include <ESP32CAN.h>
+#include <CAN_config.h>
+
+#define TX GPIO_NUM_17
+#define RX GPIO_NUM_16
+
+#endif
