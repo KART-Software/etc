74b742e1cce7ced365ab2abb5cb6052a3c04310d
ORyomei <o.ryomei1020@gmail.com>
2021-09-22 05:28:30 +0900

    Change pins

--- etc-m5stack/src/init_pins.cpp
+++ etc-m5stack/src/init_pins.cpp
@@ -2,7 +2,5 @@
 
 void initPins()
 {
-    pinMode(STEPPER_ENABLE_PIN, OUTPUT);
-    pinMode(STEPPER_DIR_PIN, OUTPUT);
-    pinMode(STEPPER_STEP_PIN, OUTPUT);
+    pinMode(SERVO_POWER_PIN, OUTPUT);
 }
--- etc-m5stack/src/init_pins.hpp
+++ etc-m5stack/src/init_pins.hpp
@@ -12,9 +12,8 @@
 #define TPS_1_PIN ARDUINO_A2
 #define TPS_2_PIN ARDUINO_A3
 
-#define STEPPER_DIR_PIN 25
-#define STEPPER_STEP_PIN 26
-#define STEPPER_ENABLE_PIN 19
+#define SERVO_POWER_PIN 19
+#define SERVO_PWM_PIN 26
 
 void initPins();
 
