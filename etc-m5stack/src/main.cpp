5ccd712b9a35634a89b86f7b0e54ab29a26d84ab
ORyomei <o.ryomei1020@gmail.com>
2021-09-22 05:31:50 +0900

    Change to servo motor ver

--- etc-m5stack/src/main.cpp
+++ etc-m5stack/src/main.cpp
@@ -1,23 +1,22 @@
 #include <M5Stack.h>
 // #include <mcp_can.h>
-#include <SPI.h>
-#include <M5Stack.h>
+// #include <SPI.h>
 
 #include "error_handler.hpp"
 #include "init_pins.hpp"
 #include "sensors.hpp"
 #include "plausibility_validator.hpp"
-#include "stepper_controller.hpp"
+#include "servo_controller.hpp"
 #include "serial_logger.hpp"
 
-TaskHandle_t stepperControllTask;
+TaskHandle_t servoControllTask;
 
 Apps apps1(APPS_1_MIN, APPS_1_MAX, APPS_1_RAW_MIN - APPS1_DIRECTION * APPS_MARGIN, APPS_1_RAW_MAX + APPS1_DIRECTION * APPS_MARGIN, APPS_1_PIN);
 Apps apps2(APPS_2_MIN, APPS_2_MAX, APPS_2_RAW_MIN - APPS2_DIRECTION * APPS_MARGIN, APPS_2_RAW_MAX + APPS2_DIRECTION * APPS_MARGIN, APPS_2_PIN);
 Tps tps1(TPS_1_MIN, TPS_1_MAX, TPS_1_RAW_MIN, TPS_1_RAW_MAX, TPS_1_PIN);
 Tps tps2(TPS_2_MIN, TPS_2_MAX, TPS_2_RAW_MIN, TPS_2_RAW_MAX, TPS_2_PIN);
 PlausibilityValidator plausibilityValidator(&apps1, &apps2, &tps1, &tps2);
-StepperController stepperController(&apps1, &apps2, &tps1, &tps2);
+ServoController servoController(&apps1, &apps2, &tps1, &tps2);
 SerialLogger serialLogger;
 unsigned long currentTime = 0;
 unsigned long lastTime = 0;
@@ -28,9 +27,9 @@ void setup()
   M5.begin();
   serialLogger.initialize();
   initPins();
-  stepperController.setStepperOn();
+  servoController.setServoOn();
   gAdc.begin();
-  xTaskCreatePinnedToCore(startStepper, "StepperConstrollTask", 8192, (void *)&stepperController, 1, &stepperControllTask, 1);
+  xTaskCreatePinnedToCore(startServo, "ServoConstrollTask", 8192, (void *)&servoController, 1, &servoControllTask, 1);
 }
 
 void loop()
@@ -44,8 +43,8 @@ void loop()
 
   if (!plausibilityValidator.isCurrentlyValid())
   {
-    stepperController.setStepperOff();
-    vTaskSuspend(stepperControllTask);
+    servoController.setServoOff();
+    vTaskSuspend(servoControllTask);
   }
   gErrorHandler.clearAll();
   serialLogger.log(
