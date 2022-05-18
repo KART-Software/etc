cc17a83b94915e5c0744d1d87fbdb2e7d67ed599
ORyomei <o.ryomei1020@gmail.com>
2022-05-18 20:41:03 +0900

    control stepper by ittr

--- etc-m5stack/src/main.cpp
+++ etc-m5stack/src/main.cpp
@@ -17,10 +17,11 @@ TaskHandle_t sensorManualAdjustTask;
 Mode mode;
 Apps apps1(APPS_1_MIN, APPS_1_MAX, APPS_MARGIN, APPS_1_RAW_MIN, APPS_1_RAW_MAX, APPS_1_PIN);
 Apps apps2(APPS_2_MIN, APPS_2_MAX, APPS_MARGIN, APPS_2_RAW_MIN, APPS_2_RAW_MAX, APPS_2_PIN);
+Ittr ittr(ITTR_MIN, ITTR_MAX, ITTR_MARGIN, ITTR_RAW_MIN, ITTR_RAW_MAX, ITTR_PIN);
 Tps tps1(TPS_1_MIN, TPS_1_MAX, TPS_MARGIN, TPS_1_RAW_MIN, TPS_1_RAW_MAX, TPS_1_PIN);
 Tps tps2(TPS_2_MIN, TPS_2_MAX, TPS_MARGIN, TPS_2_RAW_MIN, TPS_2_RAW_MAX, TPS_2_PIN);
 PlausibilityValidator plausibilityValidator(&apps1, &apps2, &tps1, &tps2);
-StepperController stepperController(&apps1, &tps1);
+StepperController stepperController(&ittr, &tps1);
 SerialLogger serialLogger;
 unsigned long currentTime = 0;
 unsigned long lastTime = 0;
@@ -45,6 +46,7 @@ void loop()
   gAdc.read();
   apps1.read();
   apps2.read();
+  ittr.read();
   tps1.read();
   tps2.read();
 
@@ -59,6 +61,7 @@ void loop()
         currentTime - lastTime,
         apps1.convertedValue(),
         apps2.convertedValue(),
+        ittr.convertedValue(),
         tps1.convertedValue(),
         tps2.convertedValue(),
         plausibilityValidator.isValid(),
@@ -78,6 +81,7 @@ void loop()
     serialLogger.log(
         apps1.getRawValue(),
         apps2.getRawValue(),
+        ittr.getRawValue(),
         tps1.getRawValue(),
         tps2.getRawValue());
     ADJUSTER_MODE_FINISH_BUTTON.read();
