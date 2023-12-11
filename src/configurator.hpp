b36b4f353fa55b1a1acd0a04292b27256ffba6e5
Osaki Ryomei <osaki.ryomei.27e@st.kyoto-u.ac.jp>
2023-12-11 16:22:39 +0900

    リファクタ

--- src/configurator.hpp
+++ src/configurator.hpp
@@ -7,7 +7,7 @@
 #include "flash.hpp"
 
 #define SENSOR_VALUES_FILE_NAME "/sensor_values.txt"
-#define RAW_SENSOR_VALUES_JSON_SIZE 256
+#define RAW_SENSOR_VALUES_JSON_SIZE 300
 
 #define WAIT_INTERVAL 1000
 #define CALIBRATE_INTERVAL 100
