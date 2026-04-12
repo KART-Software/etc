cdd1ee565807e79f35828b8eadfe6720abcc0f9e
ORyomei <o.ryomei1020@gmail.com>
2026-04-13 07:10:15 +0900

    CI : deploy to github pages

--- etc-console/vite.config.ts
+++ etc-console/vite.config.ts
@@ -3,4 +3,5 @@ import preact from "@preact/preset-vite";
 
 export default defineConfig({
   plugins: [preact({ devtoolsInProd: false, reactAliasesEnabled: false })],
+  base: process.env.VITE_BASE || "/",
 });
